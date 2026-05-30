d84f3b4e5ae4ee53fc2ce5e2489cd4c67f61a1c7
ackvanity <108710608+ackvanity@users.noreply.github.com>
2026-05-30 20:45:21 +0700

    staticfiles

--- integration/settings.py
+++ integration/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = os.getenv("STATIC_URL", "static/")
+STATIC_ROOT = os.getenv("STATIC_ROOT")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
